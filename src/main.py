eacf5190aca05daf24da9369ed036e1de91efedd
Michael Chary <mac389@gmail.com>
2014-09-10 15:20:58 -0400

    update from local on mba

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
 import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+
+filenames = {'adjacency':'../data/adj.txt'}
+
+
+adjacency_matrix = np.loadtxt(filenames['adjacency'],dtype=int)
+print adjacency_matrix.shape
+
+G = nx.from_numpy_matrix(adjacency_matrix)
+nx.draw_spring(G)
+plt.show()
